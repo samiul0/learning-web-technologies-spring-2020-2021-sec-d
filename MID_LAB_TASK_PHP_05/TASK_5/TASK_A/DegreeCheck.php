070230d0104abfcbc976f0944c7f5e11562c6a7d
SHEIKH MD SAMIUL ISLAM <77977973+samiul0@users.noreply.github.com>
2021-02-19 12:12:22 +0600

    Commit 13

---
+++ MID_LAB_TASK_PHP_05/TASK_5/TASK_A/DegreeCheck.php
@@ -0,0 +1,17 @@
+<?php
+
+	if(isset($_POST['submit']))
+	{
+
+		$degree = $_POST['degree'];
+
+		if($degree == ""){
+			echo "null submission";
+		}else{
+			echo "success...";
+		}
+	}else{
+		echo "Invalid request...";
+	}
+	
+?>
