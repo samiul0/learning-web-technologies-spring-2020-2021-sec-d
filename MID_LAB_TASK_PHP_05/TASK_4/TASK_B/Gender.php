7d1a5e5b3a5709e4a45eff3f2f60a570071923bf
SHEIKH MD SAMIUL ISLAM <77977973+samiul0@users.noreply.github.com>
2021-02-19 12:09:40 +0600

    Commit 11

---
+++ MID_LAB_TASK_PHP_05/TASK_4/TASK_B/Gender.php
@@ -0,0 +1,30 @@
+<?php
+
+	if(isset($_POST['submit']))
+	{
+		$genderr = $_POST['gender'];
+		if($gender == ""){
+			echo "null submission";
+		}else{
+			echo "success...";
+		}
+	}
+?>
+
+<html>
+<head>
+	<title>Choose Gender</title>
+</head>
+<body>
+
+	<form method="POST" action="">
+		<fieldset>
+			<legend>Gender</legend>
+			<input type="radio" name="gender"> male
+  		        <input type="radio" name="gender"> female
+  		        <input type="radio" name="gender"> other <br>
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
